1a91f54e5187f9ee264d7e62b5c77da663405ac4
David Yang <zjyang@ualberta.ca>
2017-12-12 22:03:13 -0700

    adding gallery stuff and a front-fetch for services

--- src/app/content/gallery/gallery.component.ts
+++ src/app/content/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Gallery } from '../models/gallery.interface';
 import { GALLERY_LIST } from '../mocks/gallery.mocks';
+import { FrontFetchService } from '../../service/front-fetch.service';
 
 @Component({
   selector: 'app-gallery',
@@ -9,11 +10,54 @@ import { GALLERY_LIST } from '../mocks/gallery.mocks';
 })
 export class GalleryComponent implements OnInit {
 
+  postData = {
+    'user_id': 'public'
+  };
   imageList: Gallery[];
-  constructor() {
+  imageInfo: Gallery[];
+  editOrder: boolean;
+  public responseData;
+  public galleryLoading = false;
+  public parrallaxUrl;
+  public buttonLoading = true;
+
+  constructor(public getData: FrontFetchService) {
     this.imageList = GALLERY_LIST;
   }
 
   ngOnInit() {
   }
+
+  getGalleryImages() {
+    this.getData.postData(this.postData, 'galleryImages').then((result) => {
+      this.responseData = result;
+      for (let i = 0; i < this.responseData.galleryImages.length; i++) {
+      }
+    }, (err) => {
+    });
+  }
+
+  editOrderChange(){
+    this.editOrder = true;
+    console.log(this.editOrder);
+
+    // if (this.editOrder = null){
+    //   this.editOrder = false;
+    //   console.log(this.editOrder);
+    // } else if(this.editOrder = false){
+    //   this.editOrder = true;
+    //   console.log(this.editOrder);
+    // } else if(this.editOrder = true){
+    //   this.editOrder = false;
+    //   console.log(this.editOrder);
+    // }
+  }
+
+  editOrderNumber () {
+   // alert(this.imageList.order);
+   this.editOrder = false;
+  }
+
+
+  buttonStatus(){}
 }
---
+++ src/app/service/front-fetch.service.ts
@@ -0,0 +1,35 @@
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/map';
+
+import { Injectable, OnInit } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+@Injectable()
+export class FrontFetchService {
+  // PRODUCTION OR SANDBOX DEV
+  setAs: String = 'production'; // production or sandbox
+  URL: String;
+
+  constructor(public http: Http, public router: Router) {
+    if (this.setAs === 'production') {
+      this.URL = 'https://harthousewineandtapa.com/angularServices/general/';
+    } else  if (this.setAs === 'sandbox') {
+      this.URL = 'https://harthousewineandtapa.com/ngtest/general/';
+    }
+  }
+
+  postData(credentials, type) {
+    return new Promise((resolve, reject) => {
+      const headers = new Headers();
+      this.http.post(this.URL + type, JSON.stringify(credentials), { headers: headers })
+        .subscribe(res => {
+          resolve(res.json());
+        }, (err) => {
+          reject(err);
+        });
+    });
+  }
+
+
+
+}
