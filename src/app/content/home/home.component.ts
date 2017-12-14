69ba3f55a6c16bb5150fbb83923ffeced3290ce1
James Chen <jxchen@live.ca>
2017-12-13 22:36:26 -0700

    homepage done

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -1,11 +1,11 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { FrontFetchService } from '../../service/fetch-service';
-import swal from 'sweetalert2';
 import { NotificationService } from 'app/service/notification.service';
-
+import swal from 'sweetalert2';
 
 declare var $;
 
@@ -15,12 +15,20 @@ declare var $;
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  videoContentForm: FormGroup;
   public isVideoEnabled = false;
   public toggleStatusText = 'Disabled';
   public bannerLoading = false;
+  public videoContentLoading = false;
+
   public responseData;
   public bannerImagePath;
   public s3AWSImagePath;
+
+  videoTitle;
+  videoURL;
+  contentText;
+  isDisabled;
 
   userCredentials = {
     id: sessionStorage.user_id,
@@ -33,23 +41,27 @@ export class HomeComponent implements OnInit {
     bannerID: ''
   };
 
+  toggleVideo = {
+    id: sessionStorage.user_id,
+    token: sessionStorage.token,
+    toggleStatus: <any>null
+  };
+
+  videoContent = {
+    id: sessionStorage.user_id,
+    token: sessionStorage.token,
+    videoTitle: <any>null,
+    videoURL: <any>null,
+    videoContent: <any>null
+  };
+
   constructor(private authService: FrontFetchService, public http: Http, private notification: NotificationService) {
-    $(document).ready(function () {
-      $('#summernote').summernote({
-        placeholder: 'Please write a reason here...',
-        minHeight: 250,             // set minimum height of editor
-        maxHeight: null,             // set maximum height of editor
-        focus: true,
-        toolbar: [
-          ['headline', ['style']],
-          ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript', 'strikethrough', 'clear']],
-          ['textsize', ['fontsize']],
-          ['alignment', ['ul', 'ol', 'paragraph', 'lineheight']],
-        ],
-      });
+    this.initializeHomePage();
+    this.videoContentForm = new FormGroup({
+      'videoTitle': new FormControl(this.videoTitle, [Validators.required]),
+      'videoURL': new FormControl(this.videoURL, [Validators.required])
     });
 
-    this.initializeHomePage();
   }
 
   ngOnInit() {
@@ -85,6 +97,7 @@ export class HomeComponent implements OnInit {
           $(document).ready(function () {
             $('#' + bannerDOM_ID).closest('.container').fadeOut(600);
           });
+          this.notification.showNotification('warning');
         } else {
           this.notification.showNotification('danger');
         }
@@ -102,18 +115,67 @@ export class HomeComponent implements OnInit {
     this.authService.postData(this.userCredentials, 'homePageInitialize').then((result) => {
       this.responseData = result;
       this.bannerImagePath = this.responseData.url_path;
+      this.videoTitle = this.responseData.videoContent.title;
+      this.videoURL = this.responseData.videoContent.video_url;
+      this.contentText = this.responseData.videoContent.content_text;
+      this.isDisabled = this.responseData.videoContent.isDisabled;
+
+      if (this.isDisabled === '0') {
+        this.isVideoEnabled = true;
+        this.toggleStatusText = 'Enabled';
+      } else {
+        this.isVideoEnabled = false;
+        this.toggleStatusText = 'Disabled';
+      }
+
+      try {
+        if (this.videoTitle.length > 0) {
+          const d = document.getElementById('videoTitle');
+          d.className += 'is-focused';
+        }
+        if (this.videoURL.length > 0) {
+          const c = document.getElementById('videoURL');
+          c.className += 'is-focused';
+        }
+
+      } catch ($e) {
+
+      }
+
+      // initialize summer note after everything has been rendered and get from db
+      $(document).ready(function () {
+        $('#summernote').summernote({
+          placeholder: 'Please write a reason here...',
+          minHeight: 250,             // set minimum height of editor
+          maxHeight: null,             // set maximum height of editor
+          focus: true,
+          toolbar: [
+            ['headline', ['style']],
+            ['style', ['bold', 'italic', 'underline', 'superscript', 'subscript', 'strikethrough', 'clear']],
+            ['textsize', ['fontsize']],
+            ['alignment', ['ul', 'ol', 'paragraph', 'lineheight']],
+          ],
+        });
+      });
     }, (err) => {
     });
   }
 
   homeVideoCheckBox() {
-    if (this.isVideoEnabled === true) {
-      this.isVideoEnabled = false;
-      this.toggleStatusText = 'Disabled';
-    } else if (this.isVideoEnabled === false) {
-      this.isVideoEnabled = true;
-      this.toggleStatusText = 'Enabled';
-    }
+    this.toggleVideo.toggleStatus = !this.isVideoEnabled;
+    this.authService.postData(this.toggleVideo, 'toggleVideo').then((result) => {
+      this.responseData = result;
+      if (this.isVideoEnabled === true) {
+        this.isVideoEnabled = false;
+        this.toggleStatusText = 'Disabled';
+      } else if (this.isVideoEnabled === false) {
+        this.isVideoEnabled = true;
+        this.toggleStatusText = 'Enabled';
+      }
+      this.notification.videoToggleNotification(this.isVideoEnabled);
+    }, (err) => {
+    });
+
   }
 
   uploadFile(event) {
@@ -167,6 +229,23 @@ export class HomeComponent implements OnInit {
         this.bannerLoading = false;
       }
     });
+  }
+
+  saveVideoContent(videoTitle, videoURL) {
+
+    this.videoContentLoading = true;
+    this.videoContent.videoContent = $('#summernote').summernote('code');
+
+    this.videoContent.videoTitle = videoTitle.value;
+    this.videoContent.videoURL = videoURL.value;
+
+    this.authService.postData(this.videoContent, 'videoContentSave').then((result) => {
+      this.responseData = result;
+      this.videoContentLoading = false;
+      this.notification.videoSave();
+    }, (err) => {
+    });
+
   }
 
 }
--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -4,6 +4,8 @@ declare var $;
 
 @Injectable()
 export class NotificationService {
+  messageNotify;
+  type;
 
   showNotification(type) {
     if (type === 'success') {
@@ -19,7 +21,7 @@ export class NotificationService {
             align: 'center'
           }
         });
-    } else {
+    } else if (type === 'danger') {
       $.notify({
         icon: 'notifications',
         message: 'Something went wrong!'
@@ -32,7 +34,59 @@ export class NotificationService {
             align: 'center'
           }
         });
+    } else if (type === 'warning') {
+      $.notify({
+        icon: 'notifications',
+        message: 'Image Deleted!'
+
+      }, {
+          type: type,
+          timer: 4000,
+          placement: {
+            from: 'top',
+            align: 'center'
+          }
+        });
     }
+  }
+
+  videoToggleNotification(status) {
+    if (status) {
+      this.messageNotify = 'Video Content has been enabled';
+      this.type = 'success';
+    } else {
+      this.messageNotify = 'Video Content has been disabled';
+      this.type = 'danger';
+    }
+    $.notify({
+      icon: 'notifications',
+      message: this.messageNotify
+
+    }, {
+        type: this.type,
+        timer: 4000,
+        placement: {
+          from: 'top',
+          align: 'center'
+        }
+      });
+
+  }
+
+  videoSave() {
+    $.notify({
+      icon: 'notifications',
+      message: 'Content published success!'
+
+    }, {
+        type: 'success',
+        timer: 4000,
+        placement: {
+          from: 'top',
+          align: 'center'
+        }
+      });
+
   }
 
 
